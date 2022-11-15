76013711fb3c16497287460fdefd5fcb540f851f
Jirayu <114326534+SweetsNodes@users.noreply.github.com>
2022-11-15 12:55:35 +0700

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker pull ghcr.io/pterodactyl/yolks:java_17
+docker pull ghcr.io/pterodactyl/yolks:java_16
+docker pull ghcr.io/pterodactyl/yolks:java_11
+docker pull ghcr.io/pterodactyl/yolks:java_8
+docker pull quay.io/pterodactyl/core:java-glibc
+docker pull registry.gitlab.com/tenten8401/pterodactyl-nginx
+docker pull danbothosting/aio
+docker pull ghcr.io/parkervcp/yolks:python_3.10
+docker pull ghcr.io/parkervcp/yolks:python_3.9
+docker pull ghcr.io/parkervcp/yolks:python_3.8
+docker pull ghcr.io/parkervcp/yolks:python_3.7
+docker pull ghcr.io/parkervcp/yolks:python_2.7
+docker pull ghcr.io/parkervcp/yolks:nodejs_18
+docker pull ghcr.io/parkervcp/yolks:nodejs_17
+docker pull ghcr.io/parkervcp/yolks:nodejs_16
+docker pull ghcr.io/parkervcp/yolks:nodejs_15
+docker pull ghcr.io/parkervcp/yolks:nodejs_14
+docker pull ghcr.io/parkervcp/yolks:nodejs_12
+docker pull ghcr.io/parkervcp/yolks:debian
+docker pull ghcr.io/parkervcp/yolks:mongodb_5
+docker pull ghcr.io/parkervcp/yolks:mongodb_4
+docker pull quay.io/parkervcp/pterodactyl-images:db_mariadb
+docker pull ghcr.io/parkervcp/yolks:postgres_14
+docker pull ghcr.io/parkervcp/yolks:redis_5
+docker pull ghcr.io/parkervcp/yolks:redis_6
+docker pull ghcr.io/parkervcp/yolks:redis_7
